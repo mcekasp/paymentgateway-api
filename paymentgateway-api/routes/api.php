e0648d6ae0f7794274978a66ad133d7bc1c3abaf
ginarizkaamalia <ginarizka223@gmail.com>
2022-06-02 00:35:46 +0700

    no message

--- paymentgateway-api/routes/api.php
+++ paymentgateway-api/routes/api.php
@@ -24,19 +24,17 @@ Route::get('metode_pembayaran/show/{id}', [MetodePembayaranController::class, 's
 Route::post('metode_pembayaran/update/{id}', [MetodePembayaranController::class, 'update']);
 Route::get('metode_pembayaran/destroy/{id}', [MetodePembayaranController::class, 'destroy']);
 
-<<<<<<< HEAD
 Route::get('logging', [LoggingController::class, 'index']);
 Route::post('logging/store', [LoggingController::class, 'store']);
 Route::get('logging/show/{id_logging}', [LoggingController::class, 'show']);
 Route::post('logging/update/{id_logging}', [LoggingController::class, 'update']);
-=======
+
 Route::get('pesanan', [PesananController::class, 'index']);
 Route::post('pesanan/store', [PesananController::class, 'store']);
 Route::get('pesanan/show/{id}', [PesananController::class, 'show']);
 
 Route::get('pendapatan', [PendapatanController::class, 'index']);
 Route::post('pendapatan/store', [PendapatanController::class, 'store']);
->>>>>>> origin/Gina
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
